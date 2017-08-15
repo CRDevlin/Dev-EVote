6be56162fc8869be57b1dfeab58d9c42cd04b93d
Jesus <jesus805@github.com>
2017-08-14 21:08:02 -0700

    Added more php code, added POST and GET method in vote and index php. Created admin.php

--- data/index.php
+++ data/index.php
@@ -1,17 +1,71 @@
+<?php
+session_start();
+?>
 <!doctype html>
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.5">
     <style>
     .error {color: #FF0000;}
+    .message {color: #00FF00;}
     </style>
     <title>CSUCI E-Vote</title>
 </head>
 <body>
-    <form method="post" name="submit" action="vote.php">
+    <form method="post" name="submit" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">
         <p><strong><span style="font-family:arial,helvetica,sans-serif;">Enter your token</span></strong></p>
-        <p><input maxlength="32" size="32" name="token" type="password" value="">
-        <button type="submit" id="btnSubmit" value="Submit">Submit</button>
+        <p><input maxlength="32" size="32" name="token" type="password" value=""></p>
+        <p><span class="message"><?php echo $success;?></span></p>
+        <p><span class="error"><?php echo $tokenErr;?></span></p>
+        <p><button type="submit" name="btnSubmit" value="Submit">Submit</button></p>
     </form>
+
+<?php
+function scan_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+function isValid($token) {
+    return 0
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["token"])) {
+        $tokenErr = "Token is required";
+    } else {
+        if (count($_POST["token"]) != 32) {
+            $tokenErr = "Invalid token";
+        } else {
+            //Validate token
+            $scannedToken = scan_input($_POST["token"])
+            // check if name only contains letters, numbers and whitespace
+            if (!preg_match("/^[a-zA-Z0-9]*$/", $scannedToken)) {
+                $tokenErr = "Invalid token";
+            } else {
+                //Check is token is valid
+                if(isValid($token) {
+                    $_SESSION["token"] = $scannedToken;
+                    header('Location: vote.php');
+                } else {
+                    $tokenErr = "Invalid token";
+                }
+            }
+        }
+    }
+} else if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    if(isset($_GET['result'])) {
+        $success = filter_var($_GET['result'], FILTER_VALIDATE_BOOLEAN)
+        if($result) {
+            $success = "Vote saved successfully."
+        } else {
+            $error = "Error saving vote. Please consult your administrator."
+        }
+    }
+}?>
+
+
 </body>
 </html>
--- data/vote.php
+++ data/vote.php
@@ -1,13 +1,41 @@
+<!doctype html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.5">
+    <style>
+    .error {color: #FF0000;}
+    </style>
+    <title>CSUCI E-Vote</title>
+</head>
+<body>
+    <form method="post" name="submit" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">
+        <p><strong><span style="font-family:arial,helvetica,sans-serif;">Enter your token</span></strong></p>
+        <p><input maxlength="32" size="32" name="token" type="password" value=""></p>
+        <span class="error"><?php echo $tokenErr;?></span>
+        <button type="submit" name="btnSubmit" value="Submit">Submit</button>
+    </form>
+
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty($_POST["token"])) {
-        $tokenErr = "Token is required";
-    } else if (count($_POST["token"]) != 32) {
-        $tokenErr = "Invalid token";
+
+    if (empty($_POST["choice"])) {
+        $tokenErr = "Choice is required";
     } else {
-        // check if name only contains letters, numbers and whitespace
-        if (!preg_match("/^[a-zA-Z0-9]*$/",$_POST["token"])) {
-          $tokenErr = "Invalid format";
+        $scannedToken = scan_input($_POST["token"])
+
+        if (count($scannedToken) != 32) {
+            $tokenErr = "Invalid choice";
+        } else {
+            //TODO: SAVE TO SQL SERVER
+            unset($_SESSION["token"]);
+            header('Location: index.php');
         }
+    }
+
+    function scan_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
     }
 }?>
