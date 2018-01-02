241081194733b902a461662f6c70990970942852
Jesus <jesus805@github.com>
2018-01-01 20:23:37 -0800

    Set allowed hosts to wildcard (Probably a security flaw, needs to be addressed ASAP)

--- website/website/settings.py
+++ website/website/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = '1pq&u4tbm837a(!j7*+0x^$z73f^z*xfd7!ex4k9w2jl2yllm9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
